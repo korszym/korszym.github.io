5e4ab9c79420419854e47462e1135781597e1f78
korszym <64641034+korszym@users.noreply.github.com>
2020-05-25 19:43:29 +0200

    Add files via upload

---
+++ zat.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+echo "Hello world";
+
+      ?>
+  </body>
+</html>
