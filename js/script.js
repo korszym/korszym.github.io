2ef8bbf120deebf6957470b29d9070054152709a
korszym <64641034+korszym@users.noreply.github.com>
2020-06-08 20:47:56 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,115 @@
+   
+   var ul=document.getElementById('ul');
+   var btn=document.getElementById('button');
+   var scoreCard=document.getElementById('scoreCard');
+   var quizBox=document.getElementById('questionBox');
+  var op1=document.getElementById('op1');
+  var op2=document.getElementById('op2');
+  var op3=document.getElementById('op3');
+  var op4=document.getElementById('op4');
+
+
+      var app={
+                questions:[
+                          {
+
+
+q:'Hitler w stosunku do Polski w końcu lat 30. wysunął dwa żądania. Józef Beck wyraźnie mu odmówił. Kiedy i gdzie Beck złożył oświadczenie odrzucenia żądań niemieckich?',
+
+
+
+ options:[
+
+
+
+'Na Kremlu 10 V 1931',
+
+
+
+'W Sejmie Polskim 5 V 1939',
+
+
+'W Reichstagu 9 XII 1939',
+
+
+'Wszystkie odpowiedzi są błędne'],
+
+
+answer:2},
+
+                     
+                          ],
+                index:0,
+                load:function(){
+                	   if(this.index<=this.questions.length-1){
+                        quizBox.innerHTML=this.index+1+". "+this.questions[this.index].q;      
+                        op1.innerHTML=this.questions[this.index].options[0];
+                        op2.innerHTML=this.questions[this.index].options[1];
+                        op3.innerHTML=this.questions[this.index].options[2];
+                        op4.innerHTML=this.questions[this.index].options[3];
+                           this.scoreCard();
+                        }
+                        else{
+
+                        quizBox.innerHTML="Dziękuje za udział w moim Quizie"      
+                        op1.style.display="none";
+                        op2.style.display="none";
+                        op3.style.display="none";
+                        op4.style.display="none";
+                        btn.style.display="none";
+                        }
+                },
+                 next:function(){
+                    this.index++;
+                    this.load();
+                 },
+                check:function(ele){
+                   
+                         var id=ele.id.split('');
+                         
+                         if(id[id.length-1]==this.questions[this.index].answer){
+                         	this.score++;
+                         	ele.className="prawda";
+                         	ele.innerHTML="Poprawna odpowiedź";
+                         	this.scoreCard();
+                         }
+                         else{
+                         	ele.className="Błąd";
+                         	ele.innerHTML="Błąd";
+                         }
+                },
+                notClickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="none";
+                       }
+                },
+
+                clickAble:function(){
+                       for(let i=0;i<ul.children.length;i++){
+                       	    ul.children[i].style.pointerEvents="auto";
+                       	    ul.children[i].className=''
+
+                       }
+                },
+                score:0,
+                scoreCard:function(){
+                	scoreCard.innerHTML=this.questions.length+"/"+this.score;
+                }
+ 
+           }
+
+
+           window.onload=app.load();
+
+           function button(ele){
+           	     app.check(ele);
+           	     app.notClickAble();
+           }
+
+         function  next(){
+              app.next();
+              app.clickAble();
+         } 
+
+
+
